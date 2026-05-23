4b226744a776a1451d1bd4a7e9aaf2c6b962730c
noorjadzil <noor.jadzil@gmail.com>
2026-05-23 15:27:51 +0700

    Update and rename service-worker.js to data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+export const produk = [
+  { nama:"Bubur Ori", harga:7000 },
+  { nama:"Bubur Cakwe", harga:12000 },
+  { nama:"Bubur Telur", harga:12000 },
+  { nama:"Bubur Ayam Suwir", harga:15000 },
+  { nama:"Bubur Abon", harga:17000 },
+  { nama:"Bubur Spesial", harga:13000 },
+  { nama:"Bubur Istimewa", harga:17000 },
+];
+
+export const extra = [
+  { nama:"Cakwe", harga:2000 },
+  { nama:"Ayam Suwir", harga:3000 },
+  { nama:"Abon", harga:5000 },
+  { nama:"Irisan Telur", harga:1000 },
+  { nama:"Telur Rebus", harga:1000 },
+  { nama:"Kuah", harga:500 },
+  { nama:"Krupuk", harga:500 },
+];
+
+export const minuman = [
+  { nama:"Puyuh", harga:3000 },
+  { nama:"Cleo", harga:1000 },
+  { nama:"Teh Anget", harga:3000 },
+  { nama:"Es Teh", harga:3000 },
+];
